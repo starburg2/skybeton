e0ce02d4cdd48c3db6ae3f2b72705c136df5f2b4
raAndrievskiy <81430070+raAndrievskiy@users.noreply.github.com>
2021-07-31 11:31:48 +0300

    header

--- js/script.js
+++ js/script.js
@@ -7,3 +7,26 @@ $(document).ready(function() {
     $(this).toggleClass('active').next().slideToggle(300);
   });
 });
+
+const burgerBtn = $('.burger-btn');
+const burgerBtnActive = $('.burger-btn-active');
+
+burgerBtn.on('click', function (event) {
+  event.preventDefault();
+  $('.burger').slideToggle();
+  if (!$(this).hasClass('burger-btn-active')) {
+    $(this).siblings().removeClass('burger-btn-active');
+    $(this).addClass('burger-btn-active');
+  } else {
+    $(this).removeClass('burger-btn-active');
+  }
+});
+
+$(window).scroll(function(){
+  var docscroll=$(document).scrollTop();
+  if(docscroll>$(window).height()){
+    $('burger').css({'height': $('burger').height(),'width': $('burger').width()}).addClass('fixed');
+  }else{
+    $('burger').removeClass('fixed');
+  }
+});
